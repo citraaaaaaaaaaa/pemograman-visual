2f25fbde2a4cceb6603ae2511ae805fa9fc4253a
citraaaaaaaaaaa <citranainggolan06@gmail.com>
2026-08-26 12:14:20 +0700

    pertemuan2-komponen-visual

---
+++ pertemuan2_komponen_visual/Form1.vb
@@ -0,0 +1,43 @@
+﻿Public Class Form1
+    Private Sub lblNama_Click(sender As Object, e As EventArgs) Handles lblNama.Click
+
+    End Sub
+
+    Private Sub lblNIM_Click(sender As Object, e As EventArgs) Handles lblNIM.Click
+
+    End Sub
+
+    Private Sub lblKOM_Click(sender As Object, e As EventArgs) Handles lblKOM.Click
+
+    End Sub
+
+    Private Sub btnTampilkan_Click(sender As Object, e As EventArgs) Handles btnTampilkan.Click
+        MessageBox.Show("Halo Dunia!" & vbCrLf &
+                "Nama   : " & txtNama.Text & vbCrLf &
+                "NIM    : " & txtNIM.Text & vbCrLf &
+                "KOM    : " & txtKOM.Text
+)
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles lblNIM.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblNama.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnKeluar.Click
+        Me.Close()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtNama.TextChanged
+
+    End Sub
+
+    Private Sub btnHapus_Click(sender As Object, e As EventArgs) Handles btnHapus.Click
+        txtNama.Clear()
+        txtKOM.Clear()
+        txtNIM.Clear()
+    End Sub
+End Class
