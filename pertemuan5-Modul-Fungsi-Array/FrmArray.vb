3021c654e950bff7458e751c4e3f356a9b599cb8
citraaaaaaaaaaa <citranainggolan06@gmail.com>
2026-09-16 12:19:59 +0700

    Pertemuan 5 - Modul Fungsi Array

---
+++ pertemuan5-Modul-Fungsi-Array/FrmArray.vb
@@ -0,0 +1,23 @@
+﻿Public Class FrmArray
+    Private Sub btnTampilkan_Click(sender As Object, e As EventArgs) Handles btnTampilkan.Click
+        'MessageBox.Show(Hitung(txtPanjang.Text, txtLebar.Text))
+
+        'For i As Integer = 0 To nilai.Length - 1
+        'lstNilai.Items.Add(nilai(i))
+        'Next
+
+        lstNilai.Items.Add(nilai2D(1, 2))
+    End Sub
+
+    Private Sub txtPanjang_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtPanjang.KeyPress
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtLebar_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtLebar.KeyPress
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+End Class
---
+++ pertemuan5-Modul-Fungsi-Array/ModArray.vb
@@ -0,0 +1,21 @@
+﻿Module ModArray
+    Public Sub TampilkanPesan(nama As String)
+        MessageBox.Show("Haloo " & nama)
+    End Sub
+
+    Function Hitung(panjang As Integer, lebar As Integer) As Integer
+        If panjang <= 50 Then
+            Return 2 * (panjang * lebar)
+        Else
+            Return panjang * lebar
+        End If
+    End Function
+
+    Public nilai() As Integer = {50, 60, 70, 80, 90}
+    Public nilai2D(,) As Integer = {
+        {55, 58, 60},
+        {65, 67, 69},
+        {78, 85, 90}
+    
+    }
+End Module
